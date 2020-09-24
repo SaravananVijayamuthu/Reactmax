5f6f124d6f0d71a05d404feda76287bc1fbf8546
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-24 20:08:08 +0530

    imported and used css in BuildControl.js

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,15 @@
+import classes from '*.module.sass';
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import classNames from './BuildControl.module.css';
 
 const buildControl = (props) =>(
-    <div>
-        <div>
+    <div className={classes.BuildControl}>
+        <div className={classes.label}>
         {props.label}
         </div>
-        <button>Less</button>
-        <button>More</button>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
     </div>
 );
 
