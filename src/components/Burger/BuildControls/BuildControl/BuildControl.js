fec8fff35a303c35fd61bbe1b822112d124ac350
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-24 20:00:31 +0530

    rectified err in BuildControls.js

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const buildControl = (props) =>(
+    <div>
+        <div>
+
+        </div>
+    </div>
+);
+
+export default buildControl;
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const buildcontrols = (props) => {
-    
-};
+const buildControls = (props) => (
+    <div>
 
-export default buildcontrols;
+    </div>
+);
+
+export default buildControls;
